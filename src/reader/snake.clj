12728e3b33a17e372acb62569acd63d67184a6b7
Michael Brett <mdouglasbrett@icloud.com>
2021-01-15 20:27:52 +0000

    wip: set up constants

--- src/reader/snake.clj
+++ src/reader/snake.clj
@@ -1,10 +1,22 @@
 ; START: namespace
 (ns reader.snake
-  (:import (java.awt Color Dimension) 
-	   (javax.swing JPanel JFrame Timer JOptionPane)
+  (:import (java.awt Color Dimension)
+           (javax.swing JPanel JFrame Timer JOptionPane)
            (java.awt.event ActionListener KeyListener))
   (:use examples.import-static))
+
 (import-static java.awt.event.KeyEvent VK_LEFT VK_RIGHT VK_UP VK_DOWN)
+
 ; END: namespace
 
 ; TODO: implement the Snake!
+(def width 75)
+(def height 50)
+(def point-size 10)
+(def turn-millis 75)
+(def win-length 5)
+(def dirs
+  {VK_LEFT  [-1 0]
+   VK_RIGHT [1 0]
+   VK_UP    [0 -1]
+   VK_DOWN  [0 1]})
